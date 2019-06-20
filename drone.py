6afdf6bd60ae0d30ad16c644f3f4eb8d08819411
Abhinav Pratap <abhi_sai_161@yahoo.com>
2019-06-20 21:15:09 +0100

    updated variable names, added function to control motors individually

--- drone.py
+++ drone.py
@@ -2,10 +2,10 @@ import RPi.GPIO as GPIO
 import time
 
 #set pins
-frontRight = 40 
-backRight = 38
-frontLeft = 36
-backLeft = 3
+frontRightPin = 40 # 1
+backRightPin = 38 # 2
+frontLeftPin = 36 # 3
+backLeftPin = 3 # 4
 
 aux = 13
 
@@ -18,22 +18,22 @@ GPIO.cleanup()
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(frontRight,GPIO.OUT)
-GPIO.setup(backRight,GPIO.OUT)
-GPIO.setup(frontLeft,GPIO.OUT)
-GPIO.setup(backLeft,GPIO.OUT)
+GPIO.setup(frontRightPin,GPIO.OUT)
+GPIO.setup(backRightPin,GPIO.OUT)
+GPIO.setup(frontLeftPin,GPIO.OUT)
+GPIO.setup(backLeftPin,GPIO.OUT)
 GPIO.setup(aux,GPIO.OUT)
 
-m1 = GPIO.PWM(frontRight, 50)
-m2 = GPIO.PWM(backRight, 50)
-m3 = GPIO.PWM(frontLeft, 50)
-m4 = GPIO.PWM(backLeft, 50)
+mFR = GPIO.PWM(frontRightPin, 50)
+mBR = GPIO.PWM(backRightPin, 50)
+mFL = GPIO.PWM(frontLeftPin, 50)
+mBL = GPIO.PWM(backLeftPin, 50)
 aux1 = GPIO.PWM(aux, 50)
 
-m1.start(0)
-m2.start(0)
-m3.start(0)
-m4.start(0)
+mFR.start(0)
+mBR.start(0)
+mFL.start(0)
+mBL.start(0)
 aux1.start(7)
 
 def translate(value, leftMin, leftMax, rightMin, rightMax):
@@ -47,34 +47,39 @@ def translate(value, leftMin, leftMax, rightMin, rightMax):
     # Convert the 0-1 range into a value in the right range.
     return rightMin + (valueScaled * rightSpan)
 
+    
+def setMotor(p, motor):
+    dutyCycle = translate(p, 0, 100, min_throttle, max_throttle)
+    motor.ChangeDutyCycle(dutyCycle)
+    return
+
 def arm(p):
-	dutyCycle = translate(p, 0, 100, min_throttle, max_throttle)
 	print("Arm sequence.......")
 	print("frontRight")
-	m1.ChangeDutyCycle(dutyCycle)
+	setMotor(p, mFR)
 	time.sleep(1)
 	print("backRight")
-	m2.ChangeDutyCycle(dutyCycle)
+	setMotor(p, mBR)
 	time.sleep(1)
 	print("frontLeft")
-	m3.ChangeDutyCycle(dutyCycle)
+	setMotor(p, mFL)
 	time.sleep(1)
 	print("backLeft")
-	m4.ChangeDutyCycle(dutyCycle)
+	setMotor(p, mBL)
 	time.sleep(1)
+
+def setAll(p):
+	dutyCycle = translate(p, 0, 100, min_throttle, max_throttle)
+	mFR.ChangeDutyCycle(dutyCycle)
+	mBR.ChangeDutyCycle(dutyCycle)
+	mFL.ChangeDutyCycle(dutyCycle)
+	mBL.ChangeDutyCycle(dutyCycle)
+	print("Speed: " + str(p) + "% / " + str(dutyCycle))
+	return
 
 arm(5)
 
 time.sleep(1)
-
-def set(p):
-	dutyCycle = translate(p, 0, 100, min_throttle, max_throttle)
-	m1.ChangeDutyCycle(dutyCycle)
-	m2.ChangeDutyCycle(dutyCycle)
-	m3.ChangeDutyCycle(dutyCycle)
-	m4.ChangeDutyCycle(dutyCycle)
-	print("Speed: " + str(p) + "% / " + str(dutyCycle))
-	return
 
 while True:
 	inp =  input()
@@ -84,8 +89,8 @@ while True:
 		percent -= 10
 	elif inp == "stop":
 		percent = 0
-		set(percent)
+		setAll(percent)
 		break 
-	set(percent)
+	setAll(percent)
 
 GPIO.cleanup()
