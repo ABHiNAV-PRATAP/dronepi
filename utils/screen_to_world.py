55e9fed22605e769796430b239947ab1764606ab
Yashas Ambati <yashas.amb@gmail.com>
2019-06-24 22:01:45 -0400

    class-ified screen to world

--- utils/screen_to_world.py
+++ utils/screen_to_world.py
@@ -6,36 +6,36 @@ class Point():
     y = 0
 
 
-def toRad(deg):
-    rad = deg * (math.pi/180)
-    return rad
+class World:
+    def toRad(self, deg):
+        rad = deg * (math.pi/180)
+        return rad
 
+    def getWorldCoords(self, x, y, height):
+        vfov = 60
+        frameWidth = 0
+        frameHeight = 0
+        camera_height = height
+        angle = 0
 
-def getWorldCoords(x, y):
-    vfov = 0
-    frameWidth = 0
-    frameHeight = 0
-    camera_height = 0
-    angle = 0
+        worldPoint = Point()
 
-    worldPoint = Point()
+        centerx = frameWidth/2
+        centery = frameHeight/2
+        h = x - centerx
+        k = y - centery
 
-    centerx = frameWidth/2
-    centery = frameHeight/2
-    h = x - centerx
-    k = y - centery
+        d = (frameHeight)/(2*math.tan(self.toRad(vfov/2)))
+        vtheta = math.atan(((frameHeight/2) - (frameHeight - y))/d)
+        htheta = vtheta + self.toRad(angle)
+        worldx = camera_height/math.tan(htheta)
 
-    d = (frameHeight)/(2*math.tan(toRad(vfov/2)))
-    vtheta = math.atan(((frameHeight/2) - (frameHeight - y))/d)
-    htheta = vtheta + toRad(angle)
-    worldx = camera_height/math.tan(htheta)
+        D = math.sqrt((camera_height**2) + (worldx**2))
+        ytheta = math.acos((d**2 + k**2)/(math.sqrt(d**2 + k**2) * math.sqrt(d**2 + h**2 + k**2)))
 
-    D = math.sqrt((camera_height**2) + (worldx**2))
-    ytheta = math.acos((d**2 + k**2)/(math.sqrt(d**2 + k**2) * math.sqrt(d**2 + h**2 + k**2)))
+        worldy = D * math.tan(ytheta)
 
-    worldy = D * math.tan(ytheta)
+        worldPoint.x = worldx
+        worldPoint.y = worldy
 
-    worldPoint.x = worldx
-    worldPoint.y = worldy
-
-    return worldPoint
+        return worldPoint
