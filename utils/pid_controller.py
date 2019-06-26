d2219ef191bf5dcdc5eeabd4ec6328260dbff6e1
Abhinav Pratap <19430117+ABHiNAV-PRATAP@users.noreply.github.com>
2019-06-26 15:33:26 -0400

    added PID controller

---
+++ utils/pid_controller.py
@@ -0,0 +1,22 @@
+class pid_controller:
+    def __init__(self, kP, kD, kDT):
+
+        self.kP = kP
+        self.kD = kD
+        self.kDT = kDT
+        self.desiredPos = None
+
+    def updateOutput(self, currentPos):
+        if self.desiredPos is not None:
+            return
+
+        offset = self.desiredPos - currentPos
+
+        output = (offset * self.kP) + (self.kD * ((offset - self.prevOffset) / self.kDT))
+        self.prevOffset = offset
+
+        return output
+
+    def setDesiredPos(self, pos):
+        self.desiredPos = pos
+        self.prevOffset = 0
