43f59fb13dc90e589c139610b440bfeba661f8c3
Abhinav Pratap <19430117+ABHiNAV-PRATAP@users.noreply.github.com>
2019-06-27 16:28:08 -0400

    added IMU code

--- controller.py
+++ controller.py
@@ -1,12 +1,25 @@
+from threading import Thread
+from time import sleep
+import math
+
+import utils.BerryIMU.berryIMU as IMU
 from drone import Drone
 from utils.client import Client
-from time import sleep
+from utils.pid_controller import pid_controller as PID
+import utils.BerryIMU.bmp280 as barometer
 
 throttle = 0
 MAX_THROTTLE = 10
 MAX_MSPEED = 20
 
 drone = Drone(40, 38, 36, 37, 31, MAX_THROTTLE)
+
+t_pid = PID(0.1, 0.001, 0.1)
+r_pid = PID(0.1, 0.001, 0.1)
+p_pid = PID(0.1, 0.001, 0.1)
+y_pid = PID(0.1, 0.001, 0.1)
+
+IMU.initIMU()
 
 def translate(value, leftMin, leftMax, rightMin, rightMax):
         # Figure out how 'wide' each range is
@@ -19,15 +32,17 @@ def translate(value, leftMin, leftMax, rightMin, rightMax):
         # Convert the 0-1 range into a value in the right range.
         return rightMin + (valueScaled * rightSpan)
 
+def getAltitude(bar):
+        return (10**(math.log10(barometer.read().pressure/ 1013.25)/ 5.2558797) - 1)/ (-6.8755856 * 10**-6)
+
 def get(x, y, t, yw):
         global throttle
-        global MAX_MSPEED
+        # global MAX_MSPEED
 
-        yaw = yw * MAX_MSPEED
-        pitch = y * MAX_MSPEED
-        roll = x * MAX_MSPEED
-        if t is not None:
-                throttle = t
+        yaw = y_pid.updateOutput(IMU.read().yaw, yw)
+        pitch = p_pid.updateOutput(IMU.read().pitch, y)
+        roll = r_pid.updateOutput(IMU.read().roll, x)
+        throttle = t_pid.updateOutput(getAltitude, t)
 
         compute(yaw, pitch, roll, throttle)
 
--- utils/pid_controller.py
+++ utils/pid_controller.py
@@ -5,19 +5,14 @@ class pid_controller:
         self.kD = kD
         self.kDT = kDT
         self.prevOffset = 0
-        self.desiredPos = None
 
-    def updateOutput(self, currentPos):
-        if self.desiredPos is not None:
+    def updateOutput(self, currentPos, desiredPos):
+        if desiredPos is not None:
             return
 
-        offset = self.desiredPos - currentPos
+        offset = desiredPos - currentPos
 
         output = (offset * self.kP) + (self.kD * ((offset - self.prevOffset) / self.kDT))
         self.prevOffset = offset
 
         return output
-
-    def setDesiredPos(self, pos):
-        self.desiredPos = pos
-        self.prevOffset = 0
